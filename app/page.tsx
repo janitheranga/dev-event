8f6352dd6983fdfeba5a04c0ee5350ea40839a4e
Janith Eranga <janith.pathirathna@gmail.com>
2025-12-01 07:13:41 +0530

    Events related API routes and single Event Page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { IEvent } from "@/database";
 import { events } from "@/lib/constants";
 
-const Page = () => {
+const BAESE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  const response = await fetch(`${BAESE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -15,11 +21,13 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured events</h3>
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
